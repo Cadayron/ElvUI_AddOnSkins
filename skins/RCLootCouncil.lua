20b542fa8334e1303f0afe4ebc1419e3ad7af97b
Cadayron <cadayron@live.fr>
2013-07-16 17:32:29 +0200

    Update RCLootCouncil.lua
    last version

--- skins/RCLootCouncil.lua
+++ skins/RCLootCouncil.lua
@@ -1,11 +1,13 @@
+-- for RCLootCouncil 1.5.0
 local E, L, V, P, G, _ = unpack(ElvUI)
 local AS = E:GetModule('AddOnSkins')
 local S = E:GetModule('Skins')
 
 local name = "RCLootCouncilSkin"
 function AS:SkinRCLootCouncil()
-
+	
 	--Mainframe
+	--MainFrame:SetScale(1)  --change mainframe scale here
 	AS:SkinFrame(MainFrame,"Transparent",true)
 	AS:SkinFrame(ContentFrame,"Default")
 	S:HandleButton(BtClose)
@@ -13,44 +15,47 @@ function AS:SkinRCLootCouncil()
 	if _G["BtRemove"] then S:HandleButton(BtRemove) end
 	if _G["BtClear"] then S:HandleButton(BtClear) end
 	S:HandleCheckBox(MainFrameFilterPasses)
-	S:HandleScrollBar(ContentFrameScrollBar)
-
+	S:HandleScrollBar(ContentFrameScrollBar)	
+		
 	for i = 1,10 do
-		local v = _G["ContentFrameEntry"..i.."BtVote"]
-		if v then S:HandleButton(_G["ContentFrameEntry"..i.."BtVote"])
+		if _G["ContentFrameEntry"..i.."BtVote"] then S:HandleButton(_G["ContentFrameEntry"..i.."BtVote"])
 		end
 	end	
-
+	
 	--LootFrame
-	for j = 1,10 do
-		for i = 1,8 do
-			if _G["RCLootFrameEntry"..j.."Button"..i] then S:HandleButton(_G["RCLootFrameEntry"..j.."Button"..i]) end
-		end
-	end	
+	RCLootFrame:StripTextures()
+	local function Loot_OnShow(self, event, ...)
+		for i = 1,5 do
+			if _G["RCLootFrameEntry"..i] then
+				--_G["RCLootFrameEntry"..i]:SetScale(1) --change lootframe scale here
+				AS:SkinFrame(_G["RCLootFrameEntry"..i],"Transparent",true)
+				
+				for j = 1,8 do
+					if _G["RCLootFrameEntry"..i.."Button"..j] then
+						S:HandleButton(_G["RCLootFrameEntry"..i.."Button"..j])
+					end
+				end
+			end
+		end	
+	end
 
-	for i = 1,10 do
-		if _G["RCLootFrameEntry"..i] then AS:SkinFrame(_G["RCLootFrameEntry"..i],"Transparent",true) end
-	end	
-
+	RCLootFrame:HookScript("OnShow", Loot_OnShow)
+	
 	--LootHistoryFrame
 	AS:SkinFrame(RCLootHistoryFrame)
-	AS:SkinFrame(RCLootHistoryFrameContent1,"Default")
-	AS:SkinFrame(RCLootHistoryFrameContent2Scroll,"Default")
-	RCLootHistoryFrameContent2Scroll:Size(394, 263)
-	RCLootHistoryFrameContent2Scroll:Point("TOPLEFT", RCLootHistoryFrameContent1, "TOPRIGHT", 5, 0)
-	S:HandleButton(RCLootHistoryFrameButtonAll)
+	AS:SkinFrame(RCLootHistoryFrameScrollFrame,"Default")
+	S:HandleCheckBox(RCLootHistoryFrameFilterPasses)
 	S:HandleButton(RCLootHistoryFrameButtonClose)
-	S:HandleScrollBar(RCLootHistoryFrameContent2ScrollScrollBar)
-
+	S:HandleScrollBar(RCLootHistoryFrameScrollFrameScrollBar)
+	
 	--CheckVersionFrame
 	AS:SkinFrame(RCVersionFrame)
 	AS:SkinFrame(RCVersionFrameContentFrame,"Default")
-
 	S:HandleButton(ButtonGuild)
 	S:HandleButton(RaidButton)
 	S:HandleButton(CloseButton)
 	S:HandleScrollBar(RCVersionFrameContentFrameScrollBar)
-
+	
 end
 
 AS:RegisterSkin(name, AS.SkinRCLootCouncil)
